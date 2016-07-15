b69b3dd1c3407437e401406dedc8f24dadc87102
Tony Goldsmith <tony.h.goldsmith@gmail.com>
2016-07-15 12:36:57 -0500

    got email working when a user takes a spot in a room

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -20,7 +20,7 @@ Rails.application.configure do
     address:              'smtp.gmail.com',
     port:                 587,
     domain:               'gmail.com',
-    user_name:            'tony.h.goldsmith',
+    user_name:            'tony.h.goldsmith@gmail.com',
     password:             'Purdue05',
     authentication:       'plain',
     enable_starttls_auto: true  }
