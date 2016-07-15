07a70bbb5fc6c25ffd445d1d41821d211339c4b6
Tony Goldsmith <tony.h.goldsmith@gmail.com>
2016-07-15 11:44:45 -0500

    modified git files

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -21,7 +21,7 @@ Rails.application.configure do
     port:                 587,
     domain:               'gmail.com',
     user_name:            'tony.h.goldsmith',
-    password:             'fake',
+    password:             'Purdue05',
     authentication:       'plain',
     enable_starttls_auto: true  }
   # Print deprecation notices to the Rails logger.
