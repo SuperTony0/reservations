0856c8cc858fd6212f259774413ebdb35b7c9f60
Tony Goldsmith <tony.h.goldsmith@gmail.com>
2016-07-25 19:33:20 -0500

    updated javascript files

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,22 +17,3 @@
 //= require bootstrap-sprockets
 //= require_self
 //= require_tree .
-
-
-$(document).on('ready', function() {
- $("#btn1").on("click", function(){
-   $("#travelers").show();
-   $("#rooms").hide();
-   $(this).hide(); 
-   $("#btn2").show();
- });
- $("#btn2").on("click", function() {
-   $("#rooms").show();
-   $("#travelers").hide();
-   $(this).hide(); 
-   $("#btn1").show();
- });
-});
-
-
-
--- app/assets/javascripts/trips.js
+++ app/assets/javascripts/trips.js
@@ -1,7 +1,14 @@
-$(document).on('page:load', function() {
+$(document).on('ready', function() {
  $("#btn1").on("click", function(){
-   $(this).hide();
+   $("#travelers").show();
+   $("#rooms").hide();
+   $(this).hide(); 
+   $("#btn2").show();
+ });
+ $("#btn2").on("click", function() {
+   $("#rooms").show();
+   $("#travelers").hide();
+   $(this).hide(); 
+   $("#btn1").show();
  });
 });
-
-
